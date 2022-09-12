ae9ff26d641ec6db9a4ae742c4ce852b5eb97646
Leito <leogturzi@gmail.com>
2022-09-12 10:35:44 -0300

    modificamos logica de store en groupsController

--- controllers/groupsController.js
+++ controllers/groupsController.js
@@ -5,64 +5,59 @@ const jsonTable = require('../database/jsonTable');
 const groupsModel = jsonTable('groups');
 
 module.exports = {
-    index: (req, res) => {
+  index: (req, res) => {
+    let groups = groupsModel.all();
 
-        let groups = groupsModel.all()
+    res.render('groups/index', { groups });
+  },
+  create: (req, res) => {
+    res.render('groups/create');
+  },
+  store: (req, res) => {
+    let group = { ...req.body, image: req.file.filename };
+    groupId = groupsModel.create(group);
 
-        res.render('groups/index',  { groups });
-    },
-    create: (req, res) => {
-        res.render('groups/create');
-    },
-    store: (req, res) => {
+    res.redirect('/groups/' + groupId);
+  },
+  edit: (req, res) => {
+    let group = groupsModel.find(req.params.id);
+    let categories = categoriesModel.all();
 
-        let group = req.body;
+    res.render('groups/edit', { group, categories });
+  },
+  update: (req, res) => {
+    let group = req.body;
 
-        groupId = groupsModel.create(group);
+    group.id = req.params.id;
 
-        res.redirect('/groups/' + groupId);
-    },
-    edit: (req, res) => {
-        let group = groupsModel.find(req.params.id)
-        let categories = categoriesModel.all();
+    groupId = groupsModel.update(group);
 
-        res.render('groups/edit', { group, categories });
-    },
-    update: (req, res) => {
-        let group = req.body;
+    res.redirect('/groups/' + groupId);
+  },
+  show: (req, res) => {
+    let group = groupsModel.find(req.params.id);
 
-        group.id = req.params.id;
+    res.render('groups/detail', { group });
+  },
+  destroy: (req, res) => {
+    let group = groupsModel.find(req.params.id);
+    let imagePath = path.join(__dirname, '../public/img/groups/' + group.image);
 
-        groupId = groupsModel.update(group);
+    groupsModel.delete(req.params.id);
 
-        res.redirect('/groups/' + groupId)
-    },
-    show: (req, res) => {
-        let group = groupsModel.find(req.params.id);
+    if (fs.existsSync(imagePath)) {
+      fs.unlinkSync(imagePath);
+    }
 
-        res.render('groups/detail', { group });
-    },
-    destroy: (req, res) => {
+    res.redirect('/groups');
+  },
+  search: (req, res) => {
+    // Traigo todos los grupos
 
-        let group = groupsModel.find(req.params.id);
-        let imagePath = path.join(__dirname, '../public/img/groups/' + group.image);
-        
-        groupsModel.delete(req.params.id);
+    // Filtro los grupos
 
-        if (fs.existsSync(imagePath)) {
-            fs.unlinkSync(imagePath)
-        }
+    // Envío los grupos y lo que busco el usuario a la vista
 
-        res.redirect('/groups')
-    },
-    search: (req, res) => {
-        
-        // Traigo todos los grupos
-
-        // Filtro los grupos
-
-        // Envío los grupos y lo que busco el usuario a la vista
-
-        res.render('groups/search', {});
-    },
-}
+    res.render('groups/search', {});
+  },
+};
