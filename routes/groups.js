26c37e75a9aa35915049d04a2f5aae7d9365a42a
Leito <leogturzi@gmail.com>
2022-09-12 10:34:39 -0300

    configuramos multer

--- routes/groups.js
+++ routes/groups.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer')
+const multer = require('multer');
+const path = require('path');
 const controller = require('../controllers/groupsController');
+
+// configuramos multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // metodo donde especificamos donde vamos a guardar el archivo
+    cb(null, path.join(__dirname, '../public/img/groups'));
+  },
+  filename: (req, file, cb) => {
+    // creamos variable donde guardamos el nombre del archivo
+    const newFileName = `group-${Date.now()}${path.extname(file.originalname)}`;
+    cb(null, newFileName);
+  },
+});
+
+// Ejecutamos multer
+const upload = multer({ storage });
 
 // Todos los grupos
 router.get('/', controller.index);
@@ -10,7 +27,8 @@ router.get('/', controller.index);
 router.get('/create', controller.create);
 
 // Procesamiento del formulario de creación
-router.post('/', controller.store);
+// especificamos que campo va a procesar el archivo del formulario
+router.post('/', upload.single('group-image'), controller.store);
 
 // Detalle de un grupo
 router.get('/:id', controller.show);
