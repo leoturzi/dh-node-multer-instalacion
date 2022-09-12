ef9268c2abe85dde11237b066af8c84d636ebf16
Leito <leogturzi@gmail.com>
2022-09-12 10:36:09 -0300

    formato prettier aplicado

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Configuración
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 // Rutas
-const groupsRouter = require('./routes/groups');
+const groupsRouter = require("./routes/groups");
 
-app.get('/', (req, res) => {
-    res.redirect('/groups/')
+app.get("/", (req, res) => {
+  res.redirect("/groups/");
 });
-app.use('/groups', groupsRouter);
+app.use("/groups", groupsRouter);
 
 // Servidor
-app.listen(3000, () => { console.log('Servidor funcionando en el puerto 3000.') })
+app.listen(3000, () => {
+  console.log("Servidor funcionando en el puerto 3000.");
+});
--- database/jsonTable.js
+++ database/jsonTable.js
@@ -1,71 +1,70 @@
 const fs = require('fs');
 const path = require('path');
 
-let model = function(tableName) {
-    // funcion que recibe el nombre de la tabla como parametro y devuelve un objeto
-    // con metodos para manipular la tabla
-    return {
-        filePath: path.join(__dirname, '../database/' + tableName + '.json'),
-        readFile() {
-            let fileContents = fs.readFileSync(this.filePath, 'utf8');
-            // si existe el archivo lo parsea, sino retorna un array vacio
-            if(fileContents) {
-                return JSON.parse(fileContents);
-            }
-        
-            return [];
-        },
-        writeFile(contents) {
-            let fileContents = JSON.stringify(contents, null, " ");
-            fs.writeFileSync(this.filePath, fileContents);
-        },
-        nextId() {
-            let rows = this.readFile();
-            let lastRow = rows.pop();
+let model = function (tableName) {
+  // funcion que recibe el nombre de la tabla como parametro y devuelve un objeto
+  // con metodos para manipular la tabla
+  return {
+    filePath: path.join(__dirname, '../database/' + tableName + '.json'),
+    readFile() {
+      let fileContents = fs.readFileSync(this.filePath, 'utf8');
+      // si existe el archivo lo parsea, sino retorna un array vacio
+      if (fileContents) {
+        return JSON.parse(fileContents);
+      }
 
-            if (lastRow) {
-                return ++lastRow.id;
-            }
+      return [];
+    },
+    writeFile(contents) {
+      let fileContents = JSON.stringify(contents, null, ' ');
+      fs.writeFileSync(this.filePath, fileContents);
+    },
+    nextId() {
+      let rows = this.readFile();
+      let lastRow = rows.pop();
 
-            return 1;
-        },
-        all() {
-            return this.readFile();
-        },
-        find(id) {
-            let rows = this.readFile();
-            return rows.find(row => row.id == id)
-        },
-        create(row) {
-            let rows = this.readFile();
-            row.id = this.nextId();
-            rows.push(row);
+      if (lastRow) {
+        return ++lastRow.id;
+      }
 
-            this.writeFile(rows);
+      return 1;
+    },
+    all() {
+      return this.readFile();
+    },
+    find(id) {
+      let rows = this.readFile();
+      return rows.find((row) => row.id == id);
+    },
+    create(row) {
+      let rows = this.readFile();
+      row.id = this.nextId();
+      rows.push(row);
+      this.writeFile(rows);
 
-            return row.id;
-        },
-        update(row) {
-            let rows = this.readFile();
-            let updatedRows = rows.map(oneRow => {
-                if (oneRow.id == row.id) {
-                    return row;
-                }
-
-                return oneRow;
-            }); 
-
-            this.writeFile(updatedRows);
-
-            return row.id;
-        },
-        delete(id) {
-            let rows = this.readFile();
-            let updatedRows = rows.filter(oneRow => oneRow.id != id); 
-
-            this.writeFile(updatedRows);
+      return row.id;
+    },
+    update(row) {
+      let rows = this.readFile();
+      let updatedRows = rows.map((oneRow) => {
+        if (oneRow.id == row.id) {
+          return row;
         }
-    }
-}
+
+        return oneRow;
+      });
+
+      this.writeFile(updatedRows);
+
+      return row.id;
+    },
+    delete(id) {
+      let rows = this.readFile();
+      let updatedRows = rows.filter((oneRow) => oneRow.id != id);
+
+      this.writeFile(updatedRows);
+    },
+  };
+};
 
 module.exports = model;
